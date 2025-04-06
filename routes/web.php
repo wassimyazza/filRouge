92cf62195dc4581d5917c5ca0c174f2faa8bc358
Wassim <wassimwassim.ka1@gmail.com>
2025-04-06 21:52:45 +0100

    add pages views
    add page booking-confirmation for visiteur and page profile for client

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ Route::get('/', function () {
 
 
 
+// Visiteur routers
+
 Route::get('/search', function () {
     return view('visiteur.search');
 });
@@ -20,10 +22,23 @@ Route::get('/booking', function () {
     return view('visiteur.booking');
 });
 
-Route::get('/user/dashboard', function () {
+Route::get('/booking-confirmation', function () {
+    return view('visiteur.booking-confirmation');
+});
+
+
+// Client routers
+
+Route::get('/dashboard', function () {
     return view('client.dashboard');
 });
 
+Route::get('/profile', function () {
+    return view('client.profile');
+});
+
+
+// Authentification routers
 Route::get('/login', function () {
     return view('auth.login');
 });
