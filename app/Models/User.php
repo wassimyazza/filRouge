8737dd8aee64047b8fa8d13840ea68d633b4e3a6
Wassim <wassimwassim.ka1@gmail.com>
2025-04-07 11:32:07 +0100

    upload new version laravel

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -11,58 +12,33 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
         'password',
-        'role_id',
-        'phone_number',
-        'bio',
-        'profile_picture',
     ];
 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
-
-    public function role()
-    {
-        return $this->belongsTo(Role::class);
-    }
-
-    public function hotels()
-    {
-        return $this->hasMany(Hotel::class);
-    }
-
-    public function bookings()
-    {
-        return $this->hasMany(Booking::class);
-    }
-
-    public function reviews()
-    {
-        return $this->hasMany(Review::class);
-    }
-
-    public function isAdmin()
-    {
-        return $this->role->name === 'admin';
-    }
-
-    public function isHost()
-    {
-        return $this->role->name === 'host';
-    }
-
-    public function isTraveler()
-    {
-        return $this->role->name === 'traveler';
-    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,90 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-    return view('home/home');
-});
-
-
-
-// Visiteur routers
-
-Route::get('/search', function () {
-    return view('visiteur.search');
-});
-
-Route::get('/hotel-detail', function () {
-    return view('visiteur.hotel-detail');
-});
-
-Route::get('/booking', function () {
-    return view('visiteur.booking');
-});
-
-Route::get('/booking-confirmation', function () {
-    return view('visiteur.booking-confirmation');
-});
-
-
-// Client routers
-
-Route::get('/dashboard', function () {
-    return view('client.dashboard');
-});
-
-Route::get('/profile', function () {
-    return view('client.profile');
-});
-
-
-// Authentification routers
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-Route::get('/register', function () {
-    return view('auth.register');
-});
-
-
-// Owner routers
-
-Route::get('/owner/dashboard', function(){
-    return view('owner.owner-dashboard');
-});
-
-Route::get('/owner/listings', function(){
-    return view('owner.owner-listings');
-});
-
-Route::get('/owner/add-listing', function(){
-    return view('owner.owner-add-listing');
-});
-
-Route::get('/owner/reservations', function(){
-    return view('owner.owner-reservations');
-});
-
-Route::get('/owner/reviews', function(){
-    return view('owner.owner-reviews');
-});
-
-Route::get('/owner/earnings', function(){
-    return view('owner.owner-earnings');
-});
-
-Route::get('/owner/analytics', function(){
-    return view('owner.owner-analytics');
-});
-
-Route::get('/owner/messages', function(){
-    return view('owner.owner-messages');
-});
-
-Route::get('/owner/profile', function(){
-    return view('owner.owner-profile');
-});
-
-Route::get('/owner/settings', function(){
-    return view('owner.owner-settings');
+    return view('welcome');
 });
