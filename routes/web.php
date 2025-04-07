92d875064e7c93fdb24b6e5fc364c7116af55db1
Wassim <wassimwassim.ka1@gmail.com>
2025-04-07 11:04:42 +0100

    create auth routers

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\Web\AuthController;
 use App\Http\Controllers\Web\HomeController;
+use App\Http\Controllers\ReservationController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\Web\PropertyController;
 
 /*
@@ -31,4 +35,25 @@ Route::middleware(['guest'])->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
     Route::get('/forgot-password', [AuthController::class, 'showForgotPasswordForm'])->name('password.request');
     Route::post('/forgot-password', [AuthController::class, 'forgotPassword'])->name('password.email');
+});
+
+
+// Authenticated routes
+Route::middleware(['auth'])->group(function () {
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    
+    // User profile
+    Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+    
+    // Reservations
+    Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+    
+    // Messages
+    Route::get('/messages', [MessageController::class, 'getConversationList'])->name('messages.index');
+    
+    // Transactions
+    Route::get('/transactions', [TransactionController::class, 'getTransactions'])->name('transactions.index');
+    
+
 });
